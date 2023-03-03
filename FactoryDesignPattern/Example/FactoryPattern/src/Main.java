8a3f2044f7807b8adc289b4843655251a1ea00e5
Ankur <ankurveer0522@gmail.com>
2023-03-03 23:06:27 +0530

    Factory Design Pattern

--- FactoryDesignPattern/Example/FactoryPattern/src/Main.java
+++ FactoryDesignPattern/Example/FactoryPattern/src/Main.java
@@ -4,7 +4,7 @@ import product.Shape;
 public class Main {
     public static void main(String[] args) {
         ShapeFactory shapeFactory = new ShapeFactory();
-        Shape shape = shapeFactory.getShape("triangle");
+        Shape shape = shapeFactory.getShape("rectangle");
         shape.draw();
     }
 }
